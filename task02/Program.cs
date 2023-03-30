1e33c90f63a9925925c7975a78f972d3e0c3ed1a
Andranik <andran1401@gmail.com>
2023-03-30 19:33:43 +0400

    task2

---
+++ task02/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число M");
+int M=Convert.ToInt32(Console.ReadLine()!);
+Console.WriteLine("введите число N");
+int N = Convert.ToInt32(Console.ReadLine()!);
+
+int Sum(int M,int N)
+{
+    if (M==N)return N;
+    else return M + Sum(M + 1, N);
+}
+Console.Write($" {Sum(M,N)} ");
